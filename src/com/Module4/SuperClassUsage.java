f32e40e546c4f79b0590a677de7ce342fe7b835b
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-11-14 20:01:06 +0530

    Super class contd

--- src/com/Module4/SuperClassUsage.java
+++ src/com/Module4/SuperClassUsage.java
@@ -1,14 +1,20 @@
 package com.Module4;
-
+/**
+ super keyword can be used in the following manner
+ 1) Get parent class variable
+ 2) Get parent class method
+ 3) Get parent class constructor
+ * */
 class person{
     String occupation = "None";
     int salary;
     void modifyOccupation(){
         occupation = "Changed";
+
     }
     person(){
         salary =  20000;
-        System.out.println("Parent class:" + salary);
+        System.out.println("Parent Class Constructor");
     }
 
 }
@@ -16,19 +22,22 @@ class person{
 class employee extends person{
     String occupation = "Employee";
     employee(){
-        super();
-        System.out.println("Child Class:"+occupation);
+        super(); // (3)
+        System.out.println("(3) Constructor called above using super: Child Class:"+occupation);
     }
     void modifyParentOccupation(){
-        super.modifyOccupation();
-        System.out.println(super.occupation + " and " + occupation);
+        super.salary = 10000; // (1)
+        System.out.println("(1) Salary modified using super: " + salary);
+        super.modifyOccupation();// (2)
+        System.out.println("(2) Called modifyOccupation() method using super: " + super.occupation);
+
     }
 }
 
 public class SuperClassUsage {
     public static void main(String[] args) {
         employee A = new employee();
-
+        A.modifyParentOccupation();
     }
 }
 
