55080e82e9e4f430658c9b48e0c7efdcc540cc64
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-11-14 15:09:41 +0530

    Q4 M-2 Loop Constructs modified

--- src/com/Module2/loops.java
+++ src/com/Module2/loops.java
@@ -7,20 +7,21 @@ public class loops {
 
         int [] arr = {1,2,3,4,5};
 
-
-        /* For each loop:
-         Similar to python's for x in arr */
-        System.out.print("For Each: ");
-        for (int x: arr) {
-            System.out.print(x+" ");
-        }
-        System.out.println("");
-        /* forEach function and mapToObj */
+        /* normal for loop */
         System.out.print("For Loop: ");
         for (int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
         System.out.println("");
+
+        /* Enhanced for loop
+         Similar to python's for x in arr */
+        System.out.print("Enhanced for loop: ");
+        for (int x: arr) {
+            System.out.print(x+" ");
+        }
+        System.out.println("");
+
         /* While loop */
         System.out.print("Normal While: ");
         int j = 0;
