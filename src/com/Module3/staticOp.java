21125b1f5bb239eb708a6405c41e82c6e8a6aa9a
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-11-14 20:39:47 +0530

    static and instance init

--- src/com/Module3/staticOp.java
+++ src/com/Module3/staticOp.java
@@ -1,4 +1,34 @@
 package com.Module3;
 
+/**
+ Static Keyword is used to declare a static variable
+ * Static variables and methods are class based, not object based
+ *
+ */
+
+class employee{
+    String emp_name;
+    static int count = 0;
+    employee(String emp_name){
+        // this keyword used to get reference to object
+        this.emp_name = emp_name;
+        incrementCount();
+
+    }
+    static void incrementCount(){
+        count++;
+    }
+
+    static void showCount(){
+        System.out.println(count);
+    }
+}
+
 public class staticOp {
+    public static void main(String[] args) {
+        employee A  = new employee("Emp1");
+        employee b  = new employee("Emp2");
+        employee.showCount();
+    }
+
 }
---
+++ src/com/Module3/typeInit.java
@@ -0,0 +1,33 @@
+package com.Module3;
+
+public class typeInit {
+    int a = 0;
+    // Static block works on class
+    static {
+        System.out.println("static block 1");
+    }
+
+    static {
+        System.out.println("static block 2");
+    }
+
+    typeInit(){
+        this.a = 20;
+    }
+    typeInit(int a){
+        this.a = a;
+    }
+    // Instance block works on objects
+    {
+        // Also works before constructor so a = 0 here:
+        System.out.println("Instance Initialised "+a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+        typeInit A = new typeInit();
+        typeInit B = new typeInit(25);
+        System.out.println(B.a);
+
+    }
+}
