7827b5883eb7e7990741b2c139ef9928d685d66d
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-11-14 19:53:56 +0530

    Super class

--- src/com/Module4/SuperClassUsage.java
+++ src/com/Module4/SuperClassUsage.java
@@ -1,4 +1,34 @@
 package com.Module4;
 
-public class SuperClassUsage {
+class person{
+    String occupation = "None";
+    int salary;
+    void modifyOccupation(){
+        occupation = "Changed";
+    }
+    person(){
+        salary =  20000;
+        System.out.println("Parent class:" + salary);
+    }
+
 }
+
+class employee extends person{
+    String occupation = "Employee";
+    employee(){
+        super();
+        System.out.println("Child Class:"+occupation);
+    }
+    void modifyParentOccupation(){
+        super.modifyOccupation();
+        System.out.println(super.occupation + " and " + occupation);
+    }
+}
+
+public class SuperClassUsage {
+    public static void main(String[] args) {
+        employee A = new employee();
+
+    }
+}
+
